7c0867a88b5b206567f5278d365b3abe35f4d7e4
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2021-07-20 22:55:42 +0200

    :construction: Started implementing pharmacy profile

--- Frontend/pharmacy-frontend/src/app/app-routing.module.ts
+++ Frontend/pharmacy-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PharmacyProfileComponent } from './components/pharmacy-profile/pharmacy-profile/pharmacy-profile.component';
+import { StateInputComponent } from './components/state-input/state-input/state-input.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pharmacyProfile',
+    component: PharmacyProfileComponent
+  },
+  {
+    path: 'states',
+    component: StateInputComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/pharmacy-frontend/src/app/app.module.ts
+++ Frontend/pharmacy-frontend/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PharmacyProfileComponent } from './components/pharmacy-profile/pharmacy-profile/pharmacy-profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { StateInputComponent } from './components/state-input/state-input/state-input.component';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PharmacyProfileComponent,
+    StateInputComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
