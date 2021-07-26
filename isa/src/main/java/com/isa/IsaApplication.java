f64b9e574028cd64c59beae88e919b522ccb9d04
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2021-07-26 22:30:06 +0200

    :tada: Initialized new spring boot project

--- isa/src/main/java/com/isa/IsaApplication.java
+++ isa/src/main/java/com/isa/IsaApplication.java
@@ -1,13 +1,13 @@
-package com.isa.application;
+package com.isa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class IsaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(IsaApplication.class, args);
 	}
 
 }
