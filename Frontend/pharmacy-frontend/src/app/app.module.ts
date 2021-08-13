3511b7230e80a8a9c57e126e9d0dd3669a8f385c
ognjen98 <civcic.ra167.2017@uns.ac.rs>
2021-08-13 11:10:57 +0200

    Created calendar frontend

--- Frontend/pharmacy-frontend/src/app/app.module.ts
+++ Frontend/pharmacy-frontend/src/app/app.module.ts
@@ -1,20 +1,42 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularCalendarYearViewModule } from 'angular-calendar-year-view';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import { CalendarComponent } from './calendar/calendar/calendar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AngularCalendarYearViewModule,
+    BrowserAnimationsModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    NgbModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [CalendarComponent]
 })
 export class AppModule { }
