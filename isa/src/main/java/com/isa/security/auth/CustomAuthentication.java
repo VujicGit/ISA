915d8dd57b5d0da320da0ba7cdd790704ef39738
Bojan Vujic <58082605+VujicGit@users.noreply.github.com>
2021-09-02 16:59:09 +0200

    :construction: Implemented changing password on backend

--- isa/src/main/java/com/isa/security/auth/CustomAuthentication.java
+++ isa/src/main/java/com/isa/security/auth/CustomAuthentication.java
@@ -6,10 +6,12 @@ import com.isa.user.domain.User;
 import com.isa.user.domain.enumeration.Role;
 import com.isa.user.dto.JwtAuthenticationRequest;
 import com.isa.user.exception.InvalidCredentialsException;
+import com.isa.user.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,11 +20,14 @@ public class CustomAuthentication {
     private JwtAuthenticationRequest request;
     private final AuthenticationManager authenticationManager;
     private final TokenUtils tokenUtils;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-
-    public CustomAuthentication(AuthenticationManager authenticationManager, TokenUtils tokenUtils) {
+    public CustomAuthentication(AuthenticationManager authenticationManager, TokenUtils tokenUtils, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.tokenUtils = tokenUtils;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserTokenState authenticate() {
@@ -50,6 +55,20 @@ public class CustomAuthentication {
         if(!request.getPharmacyId().equals(pharmacyAdministrator.getPharmacyId()))
             throw new InvalidCredentialsException("Invalid username or password");
         return tokenUtils.generateToken(pharmacyAdministrator.getEmail(), pharmacyAdministrator.getPharmacyId());
+    }
+
+    public void changePassword(String oldPassword, String newPassword) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String email = user.getEmail();
+
+        if(authenticationManager != null) {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, oldPassword));
+
+        } else return;
+
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+
     }
 
     public void setRequest(JwtAuthenticationRequest request) {
